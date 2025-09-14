26e2ba0fdd932bfec4184b6fa0c97db985196b25
Abduqod1rr <ttheonepersonn@gmail.com>
2025-09-14 19:46:07 +0500

    the comit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render ,HttpResponse
+from .models import Post
+from django.views.generic import DeleteView,ListView
 # Create your views here.
+
+class Homeview(ListView):
+    model=Post
+    template_name='home.html'
+    context_object_name='post'
