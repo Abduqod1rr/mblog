e628c44a0babe7a934b4f6b56b8a30961ff3698b
Abduqod1rr <ttheonepersonn@gmail.com>
2025-09-15 18:42:22 +0500

    the comit

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post=models.ForeignKey('Post',on_delete=models.CASCADE)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    comment=models.TextField()
+    comment=models.TextField(default='...')
     commented_at=models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
