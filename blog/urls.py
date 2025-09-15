f66d8a62a4cfa55292548ca9d698b6677fdb1ab8
Abduqod1rr <ttheonepersonn@gmail.com>
2025-09-15 17:55:30 +0500

    th e comit

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import Homeview
+from .views import Homeview ,AddCommentView
 from users.views import RegisterUseview ,LoginUserView ,LogoutUserVIew
 
 urlpatterns = [
     path('/',Homeview.as_view(),name='home'),
     path('register',RegisterUseview.as_view(),name='register'),
     path('login',LoginUserView.as_view(),name='login'),
-    path('logout',LogoutUserVIew.as_view(),name='logout')
+    path('logout',LogoutUserVIew.as_view(),name='logout'),
+    path('post/<int:post_pk>/comment',AddCommentView.as_view(),name='add_comment')
 
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render ,HttpResponse
-from .models import Post
-from django.views.generic import DeleteView,ListView ,CreateView
-# Create your views here.
+from django.forms import BaseModelForm
+from django.shortcuts import render ,HttpResponse,redirect
+from .models import Post ,Comment
+from django.views.generic import DeleteView,ListView ,CreateView 
+from django.contrib.auth.mixins import LoginRequiredMixin 
+from django.urls import reverse_lazy
 
 class Homeview(ListView):
     model=Post
     template_name='home.html'
     context_object_name='post'
+
+class AddCommentView(LoginRequiredMixin,CreateView):
+    model=Comment
+    fields=['comment']
+    success_url=reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user=self.request.user
+        form.instance.post=Post.objects.get(pk=self.kwargs['post_pk'])
+        return super().form_valid(form)
+        
+ 
+
 
