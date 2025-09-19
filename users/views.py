ed47e61201e7f1c6489bf55efde5f648f1678695
Abduqodir <ttheonepersonn@gmail.com>
2025-09-19 15:41:59 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -6,21 +6,21 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class RegisterUseview(FormView):
-    template_name='register.html'
-    form_class=UserCreationForm
+    template_name = 'register.html'
+    form_class = UserCreationForm
     success_url=reverse_lazy('home')
 
     def form_valid(self, form):
-        user=form.save()
+        user = form.save()
         login(self.request,user)
         return super().form_valid(form)
 
 class LoginUserView(LoginView):
-    template_name='login.html'
-    redirect_authenticated_user=True
+    template_name = 'login.html'
+    redirect_authenticated_user = True
 
     def get_success_url(self) -> str:
         return self.get_redirect_url() or reverse_lazy('home')
     
 class LogoutUserVIew(LogoutView):
-    next_page='login'
+    next_page = 'login'
